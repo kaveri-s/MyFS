0abb436576f2912d12803a0dd4b15662de0a6a62
kaveri <kaveri@sendx.io>
2018-01-27 19:06:17 +0530

    sorry, now theyre all defined :)

--- mystructs.h
+++ mystructs.h
@@ -276,18 +276,18 @@ struct mnt_namespace {
     int                 event; /* event count */
 };  
 
-struct stat {
-    unsigned int     st_dev;         /* ID of device containing file */
-    unsigned int     st_ino;         /* inode number */
-    unsigned int    st_mode;        /* protection */
-    unsigned int   st_nlink;       /* number of hard links */
-	unsigned int     st_uid;         /* user ID of owner */
-	unsigned int     st_gid;         /* group ID of owner */
-    unsigned int     st_rdev;        /* device ID (if special file) */
-    unsigned int     st_size;        /* total size, in bytes */
-    unsigned int st_blksize;     /* blocksize for filesystem I/O */
-    unsigned int  st_blocks;      /* number of 512B blocks allocated */
-    unsigned int st_atim;  /* time of last access */
-    unsigned int st_mtim;  /* time of last modification */
-	unsigned int st_ctim;  /* time of last status change */
-};
+// struct stat {
+//     unsigned int     st_dev;         /* ID of device containing file */
+//     unsigned int     st_ino;         /* inode number */
+//     unsigned int    st_mode;        /* protection */
+//     unsigned int   st_nlink;       /* number of hard links */
+// 	unsigned int     st_uid;         /* user ID of owner */
+// 	unsigned int     st_gid;         /* group ID of owner */
+//     unsigned int     st_rdev;        /* device ID (if special file) */
+//     unsigned int     st_size;        /* total size, in bytes */
+//     unsigned int st_blksize;     /* blocksize for filesystem I/O */
+//     unsigned int  st_blocks;      /* number of 512B blocks allocated */
+//     unsigned int st_atim;  /* time of last access */
+//     unsigned int st_mtim;  /* time of last modification */
+// 	unsigned int st_ctim;  /* time of last status change */
+// };
